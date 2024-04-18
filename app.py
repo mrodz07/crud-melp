2041df67db0d6f870124309c0098c7dc2a863a89
Mario Rodríguez <mrodz_contacto@proton.me>
2024-04-18 11:27:56 -0600

    Added /restaurants endpoint GET method. Now the program returns all restaurants

--- app.py
+++ app.py
@@ -1,8 +1,91 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 import sqlite3
+import json
 import csv
 
 app = Flask(__name__)
+
+DATABASE = "database.db"
+
+# TODO: Get headers from query with cursor.description
+headers = [
+    "id",
+    "rating",
+    "name",
+    "site",
+    "email",
+    "phone",
+    "street",
+    "city",
+    "state",
+    "lat",
+    "lng",
+]
+
+
+# Saves the db connection for further use
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+# Parses list data to a dict to be used in jsonify
+def parse_response(lst, headers):
+    parsed = []
+    for item in lst:
+        new_item = {}
+        # Enumerate gives the index and value of the headers array
+        for i in enumerate(headers):
+            new_item[i[1]] = item[i[0]]
+        parsed.append(new_item)
+
+    if len(parsed) > 1:
+        return json.dumps(parsed)
+    if len(parsed) == 1:
+        return json.dumps(parsed[0])
+    else:
+        return json.dumps({})
+
+
+@app.route("/restaurants", methods=["GET"])
+def get_restaurants():
+    cursor = get_db().cursor()
+    cursor.execute("SELECT * FROM Restaurants")
+    return jsonify(parse_response(cursor.fetchall(), headers))
+
+
+# @app.route("/restaurants", methods=["POST"])
+# def add_restaurant():
+#    item = request.get_json()
+#    cursor.execute("INSERT INTO restaurants VALUES (NULL, ?)", (item["name"],))
+#    conn.commit()
+#    return {"message": "Item added successfully"}, 201
+#
+#
+# @app.route("/restaurants/<int:id>", methods=["GET"])
+# def get_restaurant(id):
+#    cursor.execute("SELECT * FROM restaurants WHERE id = ?", (id,))
+#    item = cursor.fetchone()
+#    if not item:
+#        return {"message": "Item not found"}, 404
+#    return jsonify({"id": item[0], "name": item[1]})
+#
+#
+# @app.route("/restaurants/<int:id>", methods=["PUT"])
+# def update_restaurant(id):
+#    item = request.get_json()
+#    cursor.execute("UPDATE restaurants SET name = ? WHERE id = ?", (item["name"], id))
+#    conn.commit()
+#    return {"message": "Item updated successfully"}, 200
+#
+#
+# @app.route("/restaurants/<int:id>", methods=["DELETE"])
+# def delete_restaurant(id):
+#    cursor.execute("DELETE FROM restaurants WHERE id = ?", (id,))
+#    conn.commit()
+#    return {"message": "Item deleted successfully"}, 200
 
 
 if __name__ == "__main__":
